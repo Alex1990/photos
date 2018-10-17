3bb368b6356f8282db8359105f3b9bb9d141993b
Alex Chao <alexchao1990@gmail.com>
2018-10-17 22:26:42 +0800

    Build

--- dist/photos.js
+++ dist/photos.js
@@ -8,9 +8,14 @@ photosCallback(
       "location": "北京"
     },
     "images": [
-      "https://raw.githubusercontent/Alex1990/photos/master/data/Shisanling/DSC_5568.JPG",
-      "https://raw.githubusercontent/Alex1990/photos/master/data/Shisanling/DSC_5569.JPG",
-      "https://raw.githubusercontent/Alex1990/photos/master/data/Shisanling/DSC_5570.JPG"
+      "https://alex1990.github.io/photos/data/Shisanling/DSC_5568.JPG",
+      "https://alex1990.github.io/photos/data/Shisanling/DSC_5569.JPG",
+      "https://alex1990.github.io/photos/data/Shisanling/DSC_5570.JPG"
+    ],
+    "smallImages": [
+      "https://alex1990.github.io/photos/dist/Shisanling/DSC_5568.JPG",
+      "https://alex1990.github.io/photos/dist/Shisanling/DSC_5569.JPG",
+      "https://alex1990.github.io/photos/dist/Shisanling/DSC_5570.JPG"
     ]
   },
   {
@@ -21,10 +26,16 @@ photosCallback(
       "location": "北京"
     },
     "images": [
-      "https://raw.githubusercontent/Alex1990/photos/master/data/Yuanmingyuan/DSC_5836.jpg",
-      "https://raw.githubusercontent/Alex1990/photos/master/data/Yuanmingyuan/DSC_5858.jpg",
-      "https://raw.githubusercontent/Alex1990/photos/master/data/Yuanmingyuan/DSC_5869.jpg",
-      "https://raw.githubusercontent/Alex1990/photos/master/data/Yuanmingyuan/DSC_5876.jpg"
+      "https://alex1990.github.io/photos/data/Yuanmingyuan/DSC_5836.jpg",
+      "https://alex1990.github.io/photos/data/Yuanmingyuan/DSC_5858.jpg",
+      "https://alex1990.github.io/photos/data/Yuanmingyuan/DSC_5869.jpg",
+      "https://alex1990.github.io/photos/data/Yuanmingyuan/DSC_5876.jpg"
+    ],
+    "smallImages": [
+      "https://alex1990.github.io/photos/dist/Yuanmingyuan/DSC_5836.jpg",
+      "https://alex1990.github.io/photos/dist/Yuanmingyuan/DSC_5858.jpg",
+      "https://alex1990.github.io/photos/dist/Yuanmingyuan/DSC_5869.jpg",
+      "https://alex1990.github.io/photos/dist/Yuanmingyuan/DSC_5876.jpg"
     ]
   }
 ]
